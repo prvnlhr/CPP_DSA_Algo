553d20d3c3255368e0d27d9d3c17d270ead47fd9
Praveen Lohar <praveenlohar.in@gmail.com>
2022-12-03 16:21:07 +0530

    started Array problems

---
+++ DSA/Arrays/7_GFG_move_all_negatives_to_left_preserve_order.cpp
@@ -0,0 +1,236 @@
+
+/*
+>------------------------------------------------------------------------------------------------------------------------------------------------------------
+>                               █▀ ▀█▀ █▀▀ █░░ █░░ █░█ █▀█
+>                               ▄█ ░█░ ██▄ █▄▄ █▄▄ █▀█ █▀▄
+>------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+//__________________________________________________________________________________________________________________________________________________________________________________________________
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define PI 3.141592653589793238462
+
+typedef long long ll;
+
+//>---DEBUG_TEMPLATE_START---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << '{' << p.first << ", " << p.second << '}';
+}
+
+template <class T, class = decay_t<decltype(*begin(declval<T>()))>,
+          class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c)
+{
+    os << '[';
+    for (auto it = c.begin(); it != c.end(); ++it)
+        os << &", "[2 * (it == c.begin())] << *it;
+    return os << ']';
+}
+//__support up to 5 args
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, N, ...) N
+#define _FE_0(_CALL, ...)
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                           \
+    _NTH_ARG(dummy, ##__VA_ARGS__, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1, _FE_0) \
+    (MACRO, ##__VA_ARGS__)
+
+//__Change output format here
+#define out(x) #x " = " << x << "; "
+
+#ifndef ONLINE_JUDGE
+#define debug(...) \
+    cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << "\n"
+#else
+#define debug(...)
+#endif
+
+//>---DEBUG_TEMPLATE_END-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #define FOR(i, start, end) for (int i = start; i < end; i++)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define RFOR(i, start, end) for (int i = end; i >= start; i--)
+#define FOREACH(x, b) for (auto x : b)
+
+#define cout1(x) cout << x << endl;
+#define cout2(x, y) cout << x << " " << y << endl;
+#define cout3(x, y, z) cout << x << " " << y << " " << z << endl;
+
+#define SUMOFN(arr) accumulate(arr.begin(), arr.end(), 0LL);
+#define SORTASC(arr) sort(arr.begin(), arr.end());
+#define SORTDSC(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define REV(arr) reverse(arr.begin(), arr.end());
+
+#define strLow(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define strUp(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+#define lb(arr, ele) lower_bound(arr.begin(), arr.end(), ele);
+#define ub(arr, ele) upper_bound(arr.begin(), arr.end(), ele);
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef set<int> stint;
+typedef map<int, int> mpint;
+typedef pair<int, int> pi;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+//_____________________________
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+//_____________________________
+ll expo(ll a, ll b, ll mod)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+//__factorial______________________________________________
+vector<ll> fact;
+void factOfN(ll n)
+{
+    ll prod = 1;
+    fact.resize(n + 1);
+    for (int f = 1; f <= n; f++)
+    {
+
+        fact[f] = prod * f;
+        prod = prod * f;
+    }
+}
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//>----------------------------ＳＯＬＶＥ-----------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+: Time Complexity: O(N)
+: Auxiliary Space: O(1)
+! NOTE: order of elements is not mainted, so this is easy problem
+- if we were asked to mainted the order, then this would've been Hard problem
+
+*/
+
+//> NAIVE , USING EXTRA ARRAY
+//> O(N) , O(N)
+
+void moveAllNegativesToLeftNAV(vector<int> &arr)
+{
+    int n = arr.size();
+
+    vector<int> res(n);
+
+    int negIndx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            res[negIndx] = arr[i];
+            negIndx++;
+        }
+    }
+    int posIndx = negIndx++;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            res[posIndx] = arr[i];
+            posIndx++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = res[i];
+    }
+}
+
+/*
+: Time Complexity: O(n * window)
+: Auxiliary Space: O(1), since no extra space has been taken.
+https://www.geeksforgeeks.org/rearrange-positive-and-negative-numbers/
+*/
+void moveAllNegativesToLeftOP(vector<int> &arr)
+{
+
+    int n = arr.size();
+
+    int i = 0;
+    int j = 0;
+
+    while (j < n)
+    {
+        if (arr[j] >= 0)
+        {
+            j++;
+        }
+        else
+        {
+            for (int k = j; k > i; k--)
+            {
+                swap(arr[k], arr[k - 1]);
+            }
+            i++;
+            j++;
+        }
+    }
+}
+
+void solve()
+{
+    int ele;
+    vector<int> arr;
+    while (cin >> ele)
+    {
+        arr.push_back(ele);
+    }
+    debug(arr);
+    moveAllNegativesToLeftNAV(arr);
+    debug(arr);
+}
+
+//>-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+#endif
+    auto start1 = high_resolution_clock::now();
+    solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+}
