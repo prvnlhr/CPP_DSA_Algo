a2d30e6ed91681accd955800e8dbc26db3ffd659
Praveen Lohar <praveenlohar.in@gmail.com>
2022-12-10 09:48:52 +0530

    arrays: updated problem 35

--- DSA/Arrays/35_Median_of_two_sorted_array.cpp
+++ DSA/Arrays/35_Median_of_two_sorted_array.cpp
@@ -128,37 +128,53 @@ void factOfN(ll n)
 //--------------------------------------------------------------------------------------------------------------------------------
 
 //>----------------------------ＳＯＬＶＥ-----------------------------------------------------------------------------------------------------------------------------------------------
-double getMedian(vector<int> arr1, vector<int> arr2)
+/*
+* 1. Using merge technique, to make a sorted array , and finding median
+:TC: O(m+n)
+:SC: O(m+n)
+*/
+
+/*
+* 2. Using merge technique but not storing elements in array,rather
+* keeping count pointer to reach middle element and taking median
+:TC: O(m+n)
+:SC :O(1)
+*/
+
+/*
+* 3. Using Binary search appraoch
+:TC : min(logN ,LogM)
+:SC : O(1)
+*/
+double getMedianBest(vector<int> arr1, vector<int> arr2)
 {
-    if (arr2.size() > arr2.size())
+
+    if (arr1.size() > arr2.size())
     {
-        getMedian(arr2, arr1);
+        getMedianBest(arr2, arr1);
     }
 
-    int m = arr1.size();
-    int n = arr2.size();
+    int lo = 0;
+    int hi = arr1.size();
+    int n1 = arr1.size();
+    int n2 = arr2.size();
 
-    int low = 0;
-    int high = m;
-    int medianPos = ((m + n) + 1) / 2;
-
-    while (left <= right)
+    while (lo <= hi)
     {
-        int cut1 = (low + high) / 2;
-        int cut2 = medianPos - cut1;
+        int cut1 = (lo + hi) / 2;
+        int cut2 = (n1 + n2 + 1) / 2 - cut1;
 
-        int l1 = (cut1 == 0) ? INT_MIN : arr1[cut1 - 1];
-        int l2 = (cut2 == 0) ? INT_MIN : arr2[cut2 - 1];
+        int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1];
+        int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
 
-        int r1 = (cut1 == m) ? INT_MAX : arr1[cut1];
-        int r2 = (cut2 == n) ? INT_MAX : arr2[cut2];
+        int r1 = cut1 == n1 ? INT_MAX : arr1[cut1];
+        int r2 = cut2 == n2 ? INT_MAX : arr2[cut2];
 
-        if ((l1 <= r2) && (l2 <= r1))
+        if (l1 <= r2 && l2 <= r1)
         {
-            if ((m + m) % 2 == 0)
+            if ((n1 + n2) % 2 == 0)
             {
-                int ans = max(l1, l2) + min(r1, r2);
-                return ans / 2.0;
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
             }
             else
             {
@@ -167,13 +183,14 @@ double getMedian(vector<int> arr1, vector<int> arr2)
         }
         else if (l1 > r2)
         {
-            high = cut1 - 1;
+            hi = cut1 - 1;
         }
-        else if (l2 > r1)
+        else
         {
-            low = cut1 + 1;
+            lo = cut1 + 1;
         }
     }
+
     return 0.0;
 }
 
@@ -183,7 +200,7 @@ void solve()
     // vector<int> arr2{1, 2, 3, 4, 9, 11};
     vector<int> arr1{1, 2};
     vector<int> arr2{3, 4};
-    cout << getMedian(arr1, arr2) << endl;
+    cout << getMedianBest(arr1, arr2) << endl;
 }
 
 //>-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
