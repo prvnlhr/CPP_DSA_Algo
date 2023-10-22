2b518d8e395d27ee01b8db0c196a576b33aec655
Praveen Lohar <praveenlohar.in@gmail.com>
2023-10-22 19:07:03 +0530

    DP: done 51-61

---
+++ DSA/Recursion_Backtracking & DP/58_Leetcode_983_Minimum_Cost_For_Tickets.cpp
@@ -0,0 +1,169 @@
+
+/*
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+|>                               █▀ ▀█▀ █▀▀ █░░ █░░ █░█ █▀█
+|>                               ▄█ ░█░ ██▄ █▄▄ █▄▄ █▀█ █▀▄
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define PI 3.141592653589793238462
+
+typedef long long ll;
+
+// |> ---DEBUG_TEMPLATE_START---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << '{' << p.first << ", " << p.second << '}';
+}
+
+template <class T, class = decltype(begin(declval<T>())),
+          class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c)
+{
+    os << '[';
+    for (auto it = begin(c); it != end(c); ++it)
+        os << (it == begin(c) ? "" : ", ") << *it;
+    return os << ']';
+}
+
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define _FE_6(_CALL, x, ...) _CALL(x) _FE_5(_CALL, __VA_ARGS__)
+#define _FE_7(_CALL, x, ...) _CALL(x) _FE_6(_CALL, __VA_ARGS__)
+#define _FE_8(_CALL, x, ...) _CALL(x) _FE_7(_CALL, __VA_ARGS__)
+#define _FE_9(_CALL, x, ...) _CALL(x) _FE_8(_CALL, __VA_ARGS__)
+#define _FE_10(_CALL, x, ...) _CALL(x) _FE_9(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                                               \
+    _NTH_ARG(__VA_ARGS__, _FE_10, _FE_9, _FE_8, _FE_7, _FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1) \
+    (MACRO, __VA_ARGS__)
+
+//__Change output format here______________________________________________________________________________________________________________________________________________________
+#define out(x) #x " = " << x << "; "
+
+#ifndef ONLINE_JUDGE
+#define debug(...) \
+    cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << "\n"
+#else
+#define debug(...)
+#endif
+
+//|> ---DEBUG_TEMPLATE_END-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #define FOR(i, start, end) for (int i = start; i < end; i++)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define RFOR(i, start, end) for (int i = end; i >= start; i--)
+#define FOREACH(x, b) for (auto x : b)
+
+#define cout1(x) cout << x << endl;
+#define cout2(x, y) cout << x << " " << y << endl;
+#define cout3(x, y, z) cout << x << " " << y << " " << z << endl;
+
+#define SUMOFN(arr) accumulate(arr.begin(), arr.end(), 0LL);
+#define SORTASC(arr) sort(arr.begin(), arr.end());
+#define SORTDSC(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define REV(arr) reverse(arr.begin(), arr.end());
+
+#define strLow(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define strUp(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+#define lb(arr, ele) lower_bound(arr.begin(), arr.end(), ele);
+#define ub(arr, ele) upper_bound(arr.begin(), arr.end(), ele);
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef set<int> stint;
+typedef map<int, int> mpint;
+typedef pair<int, int> pi;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+//|> ---ＳＯＬＶＥ-----------------------------------------------------------------------------------------------------------------------------------------------
+
+int recursive(vector<int> &days, vector<int> &costs, int indx, int nextDay)
+{
+    if (indx >= days.size())
+    {
+        return 0;
+    }
+    if (days[indx] <= nextDay)
+    {
+        return recursive(days, costs, indx + 1, nextDay);
+    }
+    else
+    {
+        int cost1 = costs[0] + recursive(days, costs, indx + 1, days[indx]);
+        int cost2 = costs[1] + recursive(days, costs, indx + 1, days[indx] + 6);
+        int cost3 = costs[2] + recursive(days, costs, indx + 1, days[indx] + 29);
+        return min({cost1, cost2, cost3});
+    }
+}
+int recursiveMemo(vector<int> &days, vector<int> &costs, int indx, int nextDay, vector<vector<int>> &dp)
+{
+    if (indx >= days.size())
+    {
+        return 0;
+    }
+    if (dp[indx][nextDay] != -1)
+    {
+        return dp[indx][nextDay];
+    }
+    if (days[indx] <= nextDay)
+    {
+        return dp[indx][nextDay] = recursiveMemo(days, costs, indx + 1, nextDay, dp);
+    }
+    else
+    {
+        int cost1 = costs[0] + recursiveMemo(days, costs, indx + 1, days[indx], dp);
+        int cost2 = costs[1] + recursiveMemo(days, costs, indx + 1, days[indx] + 6, dp);
+        int cost3 = costs[2] + recursiveMemo(days, costs, indx + 1, days[indx] + 29, dp);
+        dp[indx][nextDay] = min({cost1, cost2, cost3});
+        return min({cost1, cost2, cost3});
+    }
+}
+int mincostTickets(vector<int> &days, vector<int> &costs)
+{
+    int d = days.size();
+    int max_days = days[d - 1] + 30;
+    vector<vector<int>> dp(d + 1, vector<int>(max_days + 1, -1));
+    return recursiveMemo(days, costs, 0, 0, dp);
+    // return recursive(days, costs, 0, 0);
+}
+void solve()
+{
+    vector<int> days{1, 4, 6, 7, 8, 20};
+    vector<int> costs{2, 7, 15};
+    int res = mincostTickets(days, costs);
+    debug(res);
+}
+
+//|> ---MAIN-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("../../Error.txt", "w", stderr);
+    freopen("../../output.txt", "w", stdout);
+    freopen("../../input.txt", "r", stdin);
+#endif
+    auto start1 = high_resolution_clock::now();
+    solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+}
