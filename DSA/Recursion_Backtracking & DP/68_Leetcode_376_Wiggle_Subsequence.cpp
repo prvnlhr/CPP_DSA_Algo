aad2e826c331f4a910bae0e68a3d24b11c11c8a0
Praveen Lohar <praveenlohar.in@gmail.com>
2023-10-25 12:48:00 +0530

    DP : 66 - 69 done

---
+++ DSA/Recursion_Backtracking & DP/68_Leetcode_376_Wiggle_Subsequence.cpp
@@ -0,0 +1,277 @@
+
+/*
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+|>                               █▀ ▀█▀ █▀▀ █░░ █░░ █░█ █▀█
+|>                               ▄█ ░█░ ██▄ █▄▄ █▄▄ █▀█ █▀▄
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define PI 3.141592653589793238462
+
+typedef long long ll;
+
+// |> ---DEBUG_TEMPLATE_START---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << '{' << p.first << ", " << p.second << '}';
+}
+
+template <class T, class = decltype(begin(declval<T>())),
+          class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c)
+{
+    os << '[';
+    for (auto it = begin(c); it != end(c); ++it)
+        os << (it == begin(c) ? "" : ", ") << *it;
+    return os << ']';
+}
+
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define _FE_6(_CALL, x, ...) _CALL(x) _FE_5(_CALL, __VA_ARGS__)
+#define _FE_7(_CALL, x, ...) _CALL(x) _FE_6(_CALL, __VA_ARGS__)
+#define _FE_8(_CALL, x, ...) _CALL(x) _FE_7(_CALL, __VA_ARGS__)
+#define _FE_9(_CALL, x, ...) _CALL(x) _FE_8(_CALL, __VA_ARGS__)
+#define _FE_10(_CALL, x, ...) _CALL(x) _FE_9(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                                               \
+    _NTH_ARG(__VA_ARGS__, _FE_10, _FE_9, _FE_8, _FE_7, _FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1) \
+    (MACRO, __VA_ARGS__)
+
+//__Change output format here______________________________________________________________________________________________________________________________________________________
+#define out(x) #x " = " << x << "; "
+
+#ifndef ONLINE_JUDGE
+#define debug(...) \
+    cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << "\n"
+#else
+#define debug(...)
+#endif
+
+//|> ---DEBUG_TEMPLATE_END-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #define FOR(i, start, end) for (int i = start; i < end; i++)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define RFOR(i, start, end) for (int i = end; i >= start; i--)
+#define FOREACH(x, b) for (auto x : b)
+
+#define cout1(x) cout << x << endl;
+#define cout2(x, y) cout << x << " " << y << endl;
+#define cout3(x, y, z) cout << x << " " << y << " " << z << endl;
+
+#define SUMOFN(arr) accumulate(arr.begin(), arr.end(), 0LL);
+#define SORTASC(arr) sort(arr.begin(), arr.end());
+#define SORTDSC(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define REV(arr) reverse(arr.begin(), arr.end());
+
+#define strLow(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define strUp(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+#define lb(arr, ele) lower_bound(arr.begin(), arr.end(), ele);
+#define ub(arr, ele) upper_bound(arr.begin(), arr.end(), ele);
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef set<int> stint;
+typedef map<int, int> mpint;
+typedef pair<int, int> pi;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+//|> ---ＳＯＬＶＥ-----------------------------------------------------------------------------------------------------------------------------------------------
+
+vector<int> res;
+int sz = 0;
+void recursive(vector<int> &nums, int i, int prev, int prevDiff, vector<int> &seq)
+{
+    debug(i);
+    if (i >= nums.size())
+    {
+        if (seq.size() > res.size())
+        {
+            debug(seq);
+            res = seq;
+            int a = res.size();
+            sz = max(sz, a);
+        }
+        return;
+    }
+    if (!prevDiff)
+    {
+        if (nums[i] - prev > 0)
+        {
+            seq.push_back(nums[i] - prev);
+            recursive(nums, i + 1, nums[i], true, seq);
+            seq.pop_back();
+        }
+        recursive(nums, i + 1, prev, prevDiff, seq);
+    }
+
+    else if (prevDiff)
+    {
+        if (nums[i] - prev < 0)
+        {
+            seq.push_back(nums[i] - prev);
+            recursive(nums, i + 1, nums[i], false, seq);
+            seq.pop_back();
+        }
+        recursive(nums, i + 1, prev, prevDiff, seq);
+    }
+}
+
+int solve(vector<int> &nums, int n, int f, int i)
+{
+
+    if (i == n)
+    {
+        return 1;
+    }
+
+    int take = 0;
+    int nottake = 0;
+    if (f == 0)
+    {
+        if (nums[i] > nums[i - 1])
+        {
+            take = 1 + solve(nums, n, 1, i + 1);
+        }
+        else
+        {
+            nottake = solve(nums, n, 0, i + 1);
+        }
+    }
+    else
+    {
+        if (nums[i] < nums[i - 1])
+        {
+            take = 1 + solve(nums, n, 0, i + 1);
+        }
+        else
+        {
+            nottake = solve(nums, n, 1, i + 1);
+        }
+    }
+
+    return max(take, nottake);
+}
+
+int wiggleMaxLength(vector<int> &nums)
+{
+    int n = nums.size();
+    int ans = solve(nums, n, 0, 1);
+    int ans2 = solve(nums, n, 1, 1);
+    return max(ans, ans2);
+}
+
+void recursiveMemo(vector<int> &nums, int i, int prev, int prevDiff, vector<int> &seq, vector<int> &dp)
+{
+    if (i >= nums.size())
+    {
+        if (seq.size() > res.size())
+        {
+            debug(seq.size());
+            res = seq;
+            int a = res.size();
+            sz = max(sz, a);
+        }
+        return;
+    }
+    if (prevDiff < 0)
+    {
+        if (dp[i] < (int)seq.size() && nums[i] - prev > 0)
+        {
+            seq.push_back(nums[i] - prev);
+            dp[i] = seq.size();
+            recursiveMemo(nums, i + 1, nums[i], nums[i] - prev, seq, dp);
+            seq.pop_back();
+        }
+        recursiveMemo(nums, i + 1, prev, prevDiff, seq, dp);
+    }
+    else if (prevDiff > 0)
+    {
+        if (dp[i] < (int)seq.size() && nums[i] - prev < 0)
+        {
+            seq.push_back(nums[i] - prev);
+            dp[i] = seq.size();
+            recursiveMemo(nums, i + 1, nums[i], nums[i] - prev, seq, dp);
+            seq.pop_back();
+        }
+        recursiveMemo(nums, i + 1, prev, prevDiff, seq, dp);
+    }
+}
+
+int wiggleMaxLength(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n <= 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int prev = nums[0];
+    vector<int> seq;
+    vector<int> dp(nums.size() + 1, -1);
+
+    // recursiveMemo(nums, 1, prev, 1, seq, dp);
+    // int a = res.size();
+    // res.clear();
+    // recursiveMemo(nums, 1, prev, -1, seq, dp);
+    // int b = res.size();
+    // return sz + 1;
+
+    recursive(nums, 1, prev, false, seq);
+    res.clear();
+    debug(res.size());
+    int a = res.size();
+    recursive(nums, 1, prev, true, seq);
+    int b = res.size();
+    debug(a, b);
+    return sz + 1;
+}
+void solve()
+{
+    vector<int> nums;
+    int ele;
+    while (cin >> ele && ele != -1)
+    {
+
+        nums.push_back(ele);
+    }
+    int ans = wiggleMaxLength(nums);
+    debug(ans);
+}
+
+//|> ---MAIN-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("../../Error.txt", "w", stderr);
+    freopen("../../output.txt", "w", stdout);
+    freopen("../../input.txt", "r", stdin);
+#endif
+    auto start1 = high_resolution_clock::now();
+    solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+}
---
+++ DSA/Recursion_Backtracking & DP/69_Leetcode_576_Out_of_Boundary_Paths.cpp
@@ -0,0 +1,170 @@
+
+/*
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+|>                               █▀ ▀█▀ █▀▀ █░░ █░░ █░█ █▀█
+|>                               ▄█ ░█░ ██▄ █▄▄ █▄▄ █▀█ █▀▄
+|>------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define PI 3.141592653589793238462
+
+typedef long long ll;
+
+// |> ---DEBUG_TEMPLATE_START---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << '{' << p.first << ", " << p.second << '}';
+}
+
+template <class T, class = decltype(begin(declval<T>())),
+          class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c)
+{
+    os << '[';
+    for (auto it = begin(c); it != end(c); ++it)
+        os << (it == begin(c) ? "" : ", ") << *it;
+    return os << ']';
+}
+
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define _FE_6(_CALL, x, ...) _CALL(x) _FE_5(_CALL, __VA_ARGS__)
+#define _FE_7(_CALL, x, ...) _CALL(x) _FE_6(_CALL, __VA_ARGS__)
+#define _FE_8(_CALL, x, ...) _CALL(x) _FE_7(_CALL, __VA_ARGS__)
+#define _FE_9(_CALL, x, ...) _CALL(x) _FE_8(_CALL, __VA_ARGS__)
+#define _FE_10(_CALL, x, ...) _CALL(x) _FE_9(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                                               \
+    _NTH_ARG(__VA_ARGS__, _FE_10, _FE_9, _FE_8, _FE_7, _FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1) \
+    (MACRO, __VA_ARGS__)
+
+//__Change output format here______________________________________________________________________________________________________________________________________________________
+#define out(x) #x " = " << x << "; "
+
+#ifndef ONLINE_JUDGE
+#define debug(...) \
+    cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << "\n"
+#else
+#define debug(...)
+#endif
+
+//|> ---DEBUG_TEMPLATE_END-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #define FOR(i, start, end) for (int i = start; i < end; i++)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define RFOR(i, start, end) for (int i = end; i >= start; i--)
+#define FOREACH(x, b) for (auto x : b)
+
+#define cout1(x) cout << x << endl;
+#define cout2(x, y) cout << x << " " << y << endl;
+#define cout3(x, y, z) cout << x << " " << y << " " << z << endl;
+
+#define SUMOFN(arr) accumulate(arr.begin(), arr.end(), 0LL);
+#define SORTASC(arr) sort(arr.begin(), arr.end());
+#define SORTDSC(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define REV(arr) reverse(arr.begin(), arr.end());
+
+#define strLow(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define strUp(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+
+#define lb(arr, ele) lower_bound(arr.begin(), arr.end(), ele);
+#define ub(arr, ele) upper_bound(arr.begin(), arr.end(), ele);
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef set<int> stint;
+typedef map<int, int> mpint;
+typedef pair<int, int> pi;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+//|> ---ＳＯＬＶＥ-----------------------------------------------------------------------------------------------------------------------------------------------
+
+int recursive(int m, int n, int maxMove, int startRow, int startColumn)
+{
+
+    debug(startRow, startColumn);
+
+    if (maxMove < 0)
+    {
+        return 0;
+    }
+    if (startRow < 0 || startRow >= m || startColumn >= n || startColumn < 0)
+    {
+        return 1;
+    }
+
+    int l = recursive(m, n, maxMove - 1, startRow, startColumn - 1);
+    int r = recursive(m, n, maxMove - 1, startRow, startColumn + 1);
+    int u = recursive(m, n, maxMove - 1, startRow - 1, startColumn);
+    int d = recursive(m, n, maxMove - 1, startRow + 1, startColumn);
+    return l + r + u + d;
+}
+
+int recursiveMemo(int m, int n, int maxMove, int startRow, int startColumn, vector<vector<vector<int>>> &dp)
+{
+
+    if (maxMove < 0)
+    {
+        return 0;
+    }
+    if (startRow < 0 || startRow >= m || startColumn >= n || startColumn < 0)
+    {
+        return 1;
+    }
+
+    if (dp[startRow][startColumn][maxMove] != -1)
+    {
+        return dp[startRow][startColumn][maxMove];
+    }
+    int l = recursiveMemo(m, n, maxMove - 1, startRow, startColumn - 1, dp) % 1000000007;
+    int r = recursiveMemo(m, n, maxMove - 1, startRow, startColumn + 1, dp) % 1000000007;
+    int u = recursiveMemo(m, n, maxMove - 1, startRow - 1, startColumn, dp) % 1000000007;
+    int d = recursiveMemo(m, n, maxMove - 1, startRow + 1, startColumn, dp) % 1000000007;
+    dp[startRow][startColumn][maxMove] = (l + r + u + d) % 1000000007;
+    return dp[startRow][startColumn][maxMove];
+}
+
+int findPaths(int m, int n, int maxMove, int startRow, int startColumn)
+{
+    vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(maxMove + 1, -1)));
+    return recursiveMemo(m, n, maxMove, startRow, startColumn, dp) % 1000000007;
+}
+void solve()
+{
+    int m, n, moves, sr, sc;
+    cin >> m >> n >> moves >> sr >> sc;
+    int ans = findPaths(m, n, moves, sr, sc);
+    debug(ans);
+}
+
+//|> ---MAIN-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("../../Error.txt", "w", stderr);
+    freopen("../../output.txt", "w", stdout);
+    freopen("../../input.txt", "r", stdin);
+#endif
+    auto start1 = high_resolution_clock::now();
+    solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+}
